b2fa5340c565bbe5da469ded765493cdcef73ee1
TAHA KAMALISADEGHIAN <tahakamali14@gmail.com>
2025-06-02 13:28:25 +0200

    template

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# WA-Stuff-Happens
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/ArqHNgsV)
+# Exam #N: "Exam Title"
+## Student: s123456 LASTNAME FIRSTNAME 
+
+## React Client Application Routes
+
+- Route `/`: page content and purpose
+- Route `/something/:param`: page content and purpose, param specification
+- ...
+
+## API Server
+
+- POST `/api/something`
+  - request parameters and request body content
+  - response body content
+- GET `/api/something`
+  - request parameters
+  - response body content
+- POST `/api/something`
+  - request parameters and request body content
+  - response body content
+- ...
+
+## Database Tables
+
+- Table `users` - contains xx yy zz
+- Table `something` - contains ww qq ss
+- ...
+
+## Main React Components
+
+- `ListOfSomething` (in `List.js`): component purpose and main functionality
+- `GreatButton` (in `GreatButton.js`): component purpose and main functionality
+- ...
+
+(only _main_ components, minor ones may be skipped)
+
+## Screenshot
+
+![Screenshot](./img/screenshot.jpg)
+
+## Users Credentials
+
+- username, password (plus any other requested info)
+- username, password (plus any other requested info)
--- server/index.mjs
+++ server/index.mjs
@@ -1,63 +1,11 @@
+// imports
 import express from 'express';
-import cors from 'cors';
-import session from 'express-session';
-import passport from 'passport';
-import { initialize } from './auth/passport-config.mjs';
-import authRoutes from './routes/auth.mjs';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+// init express
+const app = new express();
+const port = 3001;
 
-import gameRoutes from './routes/game.mjs';
-
-const app = express();
-const PORT = 3001;
-
-// Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(express.json());
-app.use(session({
-  secret: 'keyboard-cat',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-
-
-
-// DB functions
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const dbPath = path.resolve(__dirname, 'db', 'stuffhappens.sqlite');
-
-const db = await open({ filename: dbPath, driver: sqlite3.Database });
-
-const getUserByEmail = async (email) => {
-  return await db.get('SELECT * FROM users WHERE email = ?', email);
-};
-const getUserById = async (id) => {
-  return await db.get('SELECT * FROM users WHERE id = ?', id);
-};
-
-// Initialize Passport
-initialize(passport, getUserByEmail, getUserById);
-
-// Routes
-app.use('/api', authRoutes);
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Server running' });
+// activate the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
-
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
-
-
-app.use('/api', gameRoutes);
-
